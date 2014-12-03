86860d741d68ef1938de70fcc3a55a06935c4a03
Domenic Denicola <d@domenic.me>
2014-12-03 16:31:18 -0500

    Turn $ERROR into assertUnreachable

---
+++ test/cases/$error.mjsunit.js
@@ -0,0 +1,16 @@
+//
+// Array.prototype.includes stops once it hits the length of an array-like, even if there are more after
+//
+(function() {
+  var arrayLike = {
+    length: 2,
+    0: "a",
+    1: "b",
+
+    get 2() {
+      assertUnreachable("Should not try to get the second element");
+    }
+  };
+
+  assertFalse(Array.prototype.includes.call(arrayLike, "c"));
+})();
