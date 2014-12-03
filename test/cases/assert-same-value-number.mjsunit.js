21197e38ca5cfad64df224bf01db526dc696bd07
Domenic Denicola <d@domenic.me>
2014-12-03 16:25:05 -0500

    Process assert.sameValue with literals
    assert.sameValue(a, <literal>, "m") → assertEquals(<literal>, a)

---
+++ test/cases/assert-same-value-number.mjsunit.js
@@ -0,0 +1,6 @@
+//
+// Array.prototype.includes should have length 1
+//
+(function() {
+  assertEquals(1, Array.prototype.includes.length);
+})();
---
+++ test/cases/assert-same-value-string.mjsunit.js
@@ -0,0 +1,6 @@
+//
+// Array.prototype.includes should have name property with value 'includes'
+//
+(function() {
+  assertEquals("includes", Array.prototype.includes.name);
+})();
